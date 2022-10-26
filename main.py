c21d417c61fd2a934af5e254a30f8a37b6b2b58e
Maurício Gonçalves <116230925+Mauropython@users.noreply.github.com>
2022-10-26 02:19:00 +0000

    main1

---
+++ main.py
@@ -0,0 +1,26 @@
+# Meu primeiro projeto Python
+# 
+# print() = comando de saída
+print('Alo mundo !')
+
+# Quando quiser guardar uma String! (frase)
+nome = 'Maurício Gonçalves'
+idade = 30
+cpf = '09277711957'
+
+print(nome)
+print(idade)
+print(cpf)
+
+# Quando quiser exibir a frase ' Minha idade é ' completando com o conteúdo da variável idade
+
+print('Minha idade é '+ str (idade))
+print(f'Minha idade é {idade}\n')
+print('Minha idade é {}'.format(idade))
+
+
+
+'''
+Comentário em bloco
+
+'''
