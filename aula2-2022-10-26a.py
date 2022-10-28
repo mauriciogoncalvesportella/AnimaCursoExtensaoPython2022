0fd207ea467d91d6cb9e0042be45bd06553788a5
Maurício Gonçalves <116230925+Mauropython@users.noreply.github.com>
2022-10-28 01:12:16 +0000

    Ensino condicionais.

--- aula2-2022-10-26a.py
+++ aula2-2022-10-26a.py
@@ -17,6 +17,25 @@ print(f'Seu sobrenome é {sobrenome}')
 print(f'Sua idade é {idade}')
 print(f'Seu registro é {rg}')
 
+if idade >= 18:
+  print('Você é maior de idade, ótimo! Já pode beber ou dirigir')
+else:
+  print('Você é jovem ainda.')
+
+genero = input('Informe o gênero M = Masculino, F = Feminino, 0 = Outros:')
+
+if idade >= 18 and genero == 'M':
+  print('... e você também precisa/precisou prestar o serviço militar obrigatório')
+
+
+
+# == sinal de igualdade
+# E se eu quisesee perguntar o genero (M = Masculino e F = Feminino)
+# Mostre (... e você também precisa/precisou prestar o serviço militar obrigatório)
+
+# Estrutura condicional - o famoso 'SE' (if)
+# Se a pessoa for maior de idade, mostre ' Você é maior de idade, ótimo! Já pode beber e dirigir'
+
 #print(f'Boa noite, seu nome é {nome}, {sobrenome} , {idade} , {rg}')
 
 #print('Boa noite, seu nome é {}, seu sobrenome é {}, sua idade é: {} e número de registro é 123638069'.format(nome, sobrenome, idade, rg))
